786bd1c4827d69d8f196b59dbe7ea7bda7abe775
BEN DRIS Zackaria <zckben7@gmail.com>
2018-02-13 17:57:11 +0100

    Flux de tweets avec python
    fonctionne

---
+++ tweeter.py
@@ -0,0 +1,41 @@
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import json
+
+consumer_key ='HzZpvfMusPz9rOujU6XAjruBW'
+consumer_secret = 'oJ9IdjidamiZC1HZTO7gyRvrVcyHXKyaDkM7TdCOhAy3bgE7JS'
+access_token = '960464454252187653-hq9PoZZ81wisLPjt2xA6ogEgq5Ic6cG'
+access_token_secret = 'NcvGN0Lln0F4MD730krA94TFl2jlCfxbQ1v1ZcK6XAyOk'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+		# collect all desired data fields 
+        if 'text' in all_data:
+          tweet         = all_data["text"]
+          created_at    = all_data["created_at"]
+          retweeted     = all_data["retweeted"]
+          username      = all_data["user"]["screen_name"]
+          user_tz       = all_data["user"]["time_zone"]
+          user_location = all_data["user"]["location"]
+          user_coordinates   = all_data["coordinates"]
+		  
+            
+          print((username,tweet))
+          return True
+        else:
+            return True
+
+    def on_error(self, status):
+        print(status)
+
+twitterStream = Stream(auth, listener())
+#le contenu de la recherche se met ici dans track
+twitterStream.filter(track=["france"],languages = ["fr"], stall_warnings = True)
