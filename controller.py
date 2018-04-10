1671359daf89846c97ab5558fe91bdf8d234489b
BEN DRIS Zackaria <zckben7@gmail.com>
2018-04-10 13:05:47 +0200

    Methode de comparaison pour trouver les tweets avec emojis

--- controller.py
+++ controller.py
@@ -12,6 +12,9 @@ def init_tweet(valeur):
     Listener = tweeter.listener()
     twitterStream = tweeter.Stream(tweeter.auth, Listener)
     tandances = twitterStream.listener.getTrends()
-    print tandances[0]
+    print tandances[1]
     twitterStream.filter(track=[tandances[0]],languages = ["fr"], stall_warnings = True)
     liste = twitterStream.listener.retrieveList()#voici comment récupérer l'objet liste
+    print liste
+    indices = [i for i, s in enumerate(liste) if 'je' in s.lower()]#chercher les emojis avec ça
+    print indices
